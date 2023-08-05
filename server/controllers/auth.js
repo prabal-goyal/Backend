8057ab966fa3d5d27cf9921abd565a3d0adcc907
Prabal Goyal <prabalgoyal55@gmail.com>
2023-08-05 20:55:23 +0530

    commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
+const jwtsecret = "somesuperhardstring";
+
 /* REGISTER USER */
 export const register = async (req, res) => {
   try {
@@ -38,6 +40,7 @@ export const register = async (req, res) => {
   }
 };
 
+
 /* LOGGING IN */
 export const login = async (req, res) => {
   try {
@@ -48,7 +51,7 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: user._id }, jwtsecret);
     delete user.password;
     res.status(200).json({ token, user });
   } catch (err) {
